a02b9742f9162340aff53e0d7835021643105469
mahajanl <lakshamima@gmail.com>
2017-10-03 11:57:00 -0400

    loop beginnings

--- Week2Day2/Program.cs
+++ Week2Day2/Program.cs
@@ -10,25 +10,31 @@ namespace Week2Day2
     {
         static void Main(string[] args)
         {
-            string[] someNames = { "Sally", "Timmy", "Tommy", "Henry", "Susy", "Charles" };
-            Console.WriteLine(Array.IndexOf(someNames, "Henry"));
+            //string[] someNames = { "Sally", "Timmy", "Tommy", "Henry", "Susy", "Charles" };
+            //Console.WriteLine(Array.IndexOf(someNames, "Henry"));
 
-            int[] luckyNums = { 2, 4, 5, 4, 7, 9, 7, 2, 5 };
-            Console.WriteLine(Array.LastIndexOf(luckyNums, 2));
+            //int[] luckyNums = { 2, 4, 5, 4, 7, 9, 7, 2, 5 };
+            //Console.WriteLine(Array.LastIndexOf(luckyNums, 2));
 
-            char[] alpha = { 's', 'd', 'j', 'l', 'x', 'p' };
-            Console.WriteLine(alpha[0]);
-            Console.WriteLine("And once I flip it and reverse it...");
-            Array.Reverse(alpha);
-            Console.WriteLine(alpha[0]);
+            //char[] alpha = { 's', 'd', 'j', 'l', 'x', 'p' };
+            //Console.WriteLine(alpha[0]);
+            //Console.WriteLine("And once I flip it and reverse it...");
+            //Array.Reverse(alpha);
+            //Console.WriteLine(alpha[0]);
 
-            string[] studentNames = { "Sally", "Tim", "Tommy", "Henry", "Sue", "Charles", "Apple" };
-            Array.Sort(studentNames);
-            Console.WriteLine("Alphabetically speaking, the first student's name is " + studentNames[0] + " and the \nlast student's name is " + (studentNames[studentNames.Length - 1]));
+            //string[] studentNames = { "Sally", "Tim", "Tommy", "Henry", "Sue", "Charles", "Apple" };
+            //Array.Sort(studentNames);
+            //Console.WriteLine("Alphabetically speaking, the first student's name is " + studentNames[0] + " and the \nlast student's name is " + (studentNames[studentNames.Length - 1]));
             
-            int[] faveNums = { 13, 7, 14, 21, 17, 5, 15, 3, 23, 12 };
-            Array.Sort(faveNums);
-            Console.WriteLine("The lowest number of the array is " + faveNums[0] + " and the largest is " + (faveNums[faveNums.Length - 1]) + ".");
+            //int[] faveNums = { 13, 7, 14, 21, 17, 5, 15, 3, 23, 12 };
+            //Array.Sort(faveNums);
+            //Console.WriteLine("The lowest number of the array is " + faveNums[0] + " and the largest is " + (faveNums[faveNums.Length - 1]) + ".");
+
+            //for(int i = 0; i <= 10; i++)
+            //{ Console.WriteLine(i); }
+
+            //for(int counter = 50; counter <= 100; counter++)
+            //{ Console.WriteLine(counter); }
 
 
         }
